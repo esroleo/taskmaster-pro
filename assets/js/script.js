fbd7a1458b6c3063e5e40e054cfaccf7ee7c2df5
Esteban Romero <esroleo@gmail.com>
2021-01-04 12:33:57 -0500

    edit date and back to date style completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ var loadTasks = function() {
     });
   });
 
-   // Make all task editable at any time.
+    // P code to be edited //
 
    $(".list-group").on("click", "p", function() {
     console.log("<p> was clicked");
@@ -57,6 +57,7 @@ var loadTasks = function() {
     textInput.trigger("focus"); // Once the p is replaced with form and clicked, it will be highlighted
     console.log(text);
   });
+
 
   $(".list-group").on("blur", "textarea", function() {
     // get the textarea's current value/text
@@ -88,8 +89,69 @@ var loadTasks = function() {
     $(this).replaceWith(taskP);
       });
 
+    // Date code to be edited //
+
+    // due date was clicked
+  $(".list-group").on("click", "span", function() {
+    // get current text
+    var date = $(this)
+      .text()
+      .trim();
+
+    // create new input element
+    var dateInput = $("<input>")
+      .attr("type", "text")
+      .addClass("form-control")
+      .val(date);
+
+    // swap out elements
+    $(this).replaceWith(dateInput);
+
+    // automatically focus on new element
+    dateInput.trigger("focus");
+
+
+  });
+
+  // Date code to be edited and change back to date //
+
+  // value of due date was changed
+  $(".list-group").on("blur", "input[type='text']", function() {
+    // get current text
+    var date = $(this)
+      .val()
+      .trim();
+
+    // get the parent ul's id attribute
+    var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
+
+    // get the task's position in the list of other li elements
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
+
+    // update task in array and re-save to localstorage
+    tasks[status][index].date = date;
+    saveTasks();
+
+    // recreate span element with bootstrap classes
+    var taskSpan = $("<span>")
+      .addClass("badge badge-primary badge-pill")
+      .text(date);
+
+    // replace input with span element
+    $(this).replaceWith(taskSpan);
+  });
+
+
+
 
 };
+
+
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
