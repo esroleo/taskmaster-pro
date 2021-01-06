7f19a2e56674c53d56e96a87fbffffe97f1d8087
Esteban Romero <esroleo@gmail.com>
2021-01-05 21:12:33 -0500

    modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   //debugger;
   // create elements that make up a task item
-  var taskLi = $("<li>").addClass("list-group-item");
+  var taskLi = $("<li>").addClass("list-group-item list-group-flush");
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
