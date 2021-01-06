9ed1fca6874f4cda6146e6ddb6950c8226ce699c
Esteban Romero <esroleo@gmail.com>
2021-01-06 15:02:15 -0500

    task auditing with moment.js and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 var tasks = {};
 
+var auditTask = function(taskEl) {
+  // to ensure element is getting to the function
+  // console.log(taskEl);
+    // get date from task element
+    var date = $(taskEl).find("span").text().trim();
+    // ensure it worked
+    //console.log(date); 
+  
+    // convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+    // this should print out an object for the value of the date variable, but at 5:00pm of that date
+    //console.log(time);
+
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+      // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+
+
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,8 +39,15 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // moment.js time auditing
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+
+  
+
 };
 
 var loadTasks = function() {
@@ -190,6 +223,12 @@ $(".list-group").on("blur", "textarea", function() {
 
   // replace textarea with new content
   $(this).replaceWith(taskP);
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
+  $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // due date was clicked inside the created task for edit
@@ -277,3 +316,4 @@ loadTasks();
 $("#modalDueDate").datepicker({
   minDate: 1 // Modify the object datepicker mindDate = 1 to avoid picking up the past date
 });
+
